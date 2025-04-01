60f968caed4244748d7076ed19f385c6c14de364
KubaPC <ivo.urbanski@gmail.com>
2025-04-01 09:31:36 +0200

    prod/dev prisma enviros

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,17 +1,3 @@
-// let PrismaClientConstructor;
-//
-// if (process.env.NODE_ENV === "production") {
-//     // Adjust the path as necessary
-//     // Use your production client generated in prisma/generated/prod-client
-//     PrismaClientConstructor = require("/prisma/generated/prod-client").PrismaClient;
-// } else {
-//     // Use your development client generated in prisma/generated/dev-client
-//     PrismaClientConstructor = require("/prisma/generated/dev-client").PrismaClient;
-// }
-//
-// const prisma = new PrismaClientConstructor();
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
