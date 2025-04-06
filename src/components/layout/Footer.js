05fe8d46cde6830a8d292a3e94e93853eb116ead
KubaPC <ivo.urbanski@gmail.com>
2025-04-06 13:40:49 +0200

    user orders fix

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="bg-card-bg border-t border-accent mt-auto transition-colors duration-300">
             <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
                 <p className="text-center text-sm text-neutral transition-colors duration-300">
-                    © {new Date().getFullYear()} TableMate. All rights reserved.
+                    © {new Date().getFullYear()} Lab Dynamics. All rights reserved.
                 </p>
             </div>
         </footer>
